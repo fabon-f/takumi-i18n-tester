5ed4511aaa155ccdf862a0b6a564aa4dd4f7acec
fabon <syobon.hinata.public@gmail.com>
2026-05-05 07:13:10 +0900

    Fix Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   // hack to avoid Satori error
   // https://github.com/vercel/satori/issues/738
   define: {
